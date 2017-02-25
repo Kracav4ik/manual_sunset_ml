bab1935d7f9b7af0ce2c29baa0e6846863ea8477
Kracav4ik <dikama2013@yandex.ru>
2017-02-25 16:13:11 +0300

    Multiply matrices works with other numbers!

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ int main(int argc, char* argv[]) {
 //    QApplication app(argc, argv);
 //    MainWindow* window = new MainWindow();
 //    return app.exec();
-    Matrix m1(3,1);
-    Matrix m2(1,3);
+    Matrix m1({{1},{0},{3}});
+    Matrix m2({{0, 5, 6}});
+    Matrix m3(3,1);
+    Matrix m4(1,3);
 
-    printf("%s", (m2*m1).str().toUtf8().toStdString().c_str());
+    printf("%s\n", (m1*m2).str().toUtf8().toStdString().c_str());
+    printf("%s\n", (m3*m4).str().toUtf8().toStdString().c_str());
 
     return 0;
 }
