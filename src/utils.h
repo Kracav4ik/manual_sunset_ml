592cdfb12d0ca1c1ddd459743c355cf17201328d
Kracav4ik <dikama2013@yandex.ru>
2017-03-05 17:58:58 +0300

    Python-like range()

--- src/utils.h
+++ src/utils.h
@@ -7,3 +7,24 @@ float frand();
 float frand(float from, float to);
 
 void crashForDebug();
+
+class range {
+public:
+    range(int val) : val(val) {}
+
+    class range_iter {
+    public:
+        range_iter(int i) : i(i) {}
+        int operator*() const { return i; }
+        bool operator !=(const range_iter& other) { return i != other.i; }
+        void operator++() { ++i; }
+    private:
+        int i;
+    };
+
+    range_iter begin() const { return 0; }
+    range_iter end() const { return val; }
+private:
+    int val;
+};
+
